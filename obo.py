2864340c1333723a43d1a502ea5dc3f542fcbac5
katabasist <claire.anderson@tufts.edu>
2025-05-10 02:14:46 +0000

    update obo.py

--- obo.py
+++ obo.py
@@ -33,3 +33,9 @@ def stripNonAlphaNum(text):
 def wordListToFreqDict(wordlist):
     wordfreq = [wordlist.count(p) for p in wordlist]
     return dict(list(zip(wordlist,wordfreq)))
+
+def sortFreqDict(freqdict):
+    aux = [(freqdict[key], key) for key in freqdict]
+    aux.sort()
+    aux.reverse()
+    return aux
