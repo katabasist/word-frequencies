d1854472edd07b5ddf5a5e6457a17d8b237d4eb0
katabasist <claire.anderson@tufts.edu>
2025-05-10 02:21:03 +0000

    idk!!!

--- obo.py
+++ obo.py
@@ -34,6 +34,7 @@ def wordListToFreqDict(wordlist):
     wordfreq = [wordlist.count(p) for p in wordlist]
     return dict(list(zip(wordlist,wordfreq)))
 
+
 def sortFreqDict(freqdict):
     aux = [(freqdict[key], key) for key in freqdict]
     aux.sort()
